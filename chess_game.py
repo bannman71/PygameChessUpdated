9d37f8f5491f9638d328ef236515501056ea575f
Max <maxs29@icloud.com>
2022-05-28 19:59:33 +0100

    Placing pieces on coords has started

--- chess_game.py
+++ chess_game.py
@@ -1,15 +1,21 @@
 import string
+from tkinter.tix import IMAGE
 import pygame as pg
 import numpy as np
+import random
 
 BLACK = (118, 150, 86)
 WHITE = (238, 238, 210)
 BOARD_SIZE = 8
 # makes window size a mulitple of board size (8)
-WINDOW_SIZE = (800 // BOARD_SIZE) * BOARD_SIZE
-IMAGES = [0]*12
-
+WINDOW_SIZE = (600 // BOARD_SIZE) * BOARD_SIZE
+BLOCK_SIZE = WINDOW_SIZE / BOARD_SIZE
 FEN_STARTING_BOARD = str("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+IMAGES = {}
+
+# (9,10) is the center of square 1 with the blocksize being 75
+#9 , 84, 157.5
+#10, 85,
 
 
 class Player:
@@ -22,7 +28,7 @@ class Player:
 
 class Position:
 
-    _returned_position = [[0]*8]*8
+    _returned_position = np.array([9, 9], dtype='S1')
 
     # //////
     # if constructor is empty then the starting position is made
@@ -30,11 +36,11 @@ class Position:
     # //////
 
     def __init__(self, FEN_string: string):
-        self._returned_position = FEN_STARTING_BOARD
+        self._returned_position = Position.FEN_to_board(FEN_string)
 
     def __init__(self):  # if no constructor
         self._returned_position = Position.FEN_to_board(
-            'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/')
+            FEN_STARTING_BOARD)
 
     def FEN_to_board(FEN: string):
         temp_board = np.zeros([9, 9], dtype='S1')
@@ -79,9 +85,9 @@ class Position:
 
 class Board:
 
-    _board = [[0]*8]*8
+    _board = np.zeros([9, 9], dtype='S1')
 
-    def __init__(self, Position: Position):
+    def __init__(self, Position):  # pass in a 2d array for position
         self._board = Position
 
     def square_location(self):
@@ -94,28 +100,28 @@ class Graphics:
         pass
 
     def load_images():
-        index = 0
+       # index = 0
         for im in Player.Pieces:
-            IMAGES[index] = pg.transform.smoothscale(
-                pg.image.load("/Users/maxscullion/Projects/PygameChess/classic_hq/" + im + ".png"), (100, 100))
-            index += 1
+            IMAGES[im] = (pg.transform.smoothscale(
+                pg.image.load("/Users/maxscullion/Projects/PygameChess/classic_hq/" + im + ".png"), (BLOCK_SIZE * 0.75, BLOCK_SIZE * 0.75)))
+            #index += 1
 
     def draw_grid():
-        blocksize = WINDOW_SIZE / BOARD_SIZE
         for y in range(0, BOARD_SIZE):
             for x in range(0, BOARD_SIZE//2):
                 rect = pg.Rect(
-                    (x*2 + ((y + 1) % 2)) * blocksize, y*blocksize, blocksize, blocksize)
+                    (x*2 + ((y + 1) % 2)) * BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
                 pg.draw.rect(SCREEN, WHITE, rect, 64)
 
     # draw_piece('r',x,y)
 
-    def draw_piece(self, colour, piece_type, ):
-        if colour == 'b':
-            pass
+    def draw_piece(colour, piece_name, xytuple):
+        x, y = xytuple
 
-# is lower case so b_
-# search for
+        if float.is_integer((x+66) / 75) == False or float.is_integer((y + 65) / 75) == False:
+            raise Exception('Must be a valid coordinate')
+
+        SCREEN.blit(IMAGES[colour[0] + '_' + piece_name], (x, y))
 
 
 if __name__ == "__main__":
@@ -125,23 +131,23 @@ if __name__ == "__main__":
     SCREEN = pg.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
     SCREEN.fill(BLACK)
 
-    print(Position()._returned_position[2][2])
-
-    for x in range(1, 9):
-        for y in range(1, 9):
-            print(Position()._returned_position[x][y], end=' ')
-        print()
-
     run = True
-    while run:
-        Graphics.draw_grid()
-        Graphics.load_images()
-        #help = Graphics.test()
-        Board(0).square_location
 
-        SCREEN.blit(IMAGES[2], (25, 25))
+    Graphics.draw_grid()
+    Graphics.load_images()
+
+    Graphics.draw_piece('b', 'queen', (9, 160))
+
+    while run:
+
+        #help = Graphics.test()
+        # Board(0).square_locatio
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
+
+            if event.type == pg.MOUSEBUTTONDOWN:
+                x, y = event.pos
 
         pg.display.update()
---
+++ help.py
@@ -0,0 +1,12 @@
+from pickletools import int4
+import string
+import numpy as np
+from pyparsing import Char
+
+
+temp_board = np.zeros([9, 9], dtype='S3')
+temp_board[0, 0] = 'hlkj'
+print(temp_board)
+
+
+print(float.is_integer((157.5 + 67.5) / 75))
